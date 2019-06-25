72742c984de9223a3da49ac9579df9943701d324
sstamenovcode <stamenov90@gmail.com>
2019-06-25 15:04:13 +0300

    first commit

---
+++ src/actions/postActions.js
@@ -0,0 +1,12 @@
+import { FETCH_POSTS, NEW_POST } from './types'
+
+export function fetchPosts() {
+  return function(dispatch) {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => dispatch({ 
+        type: FETCH_POSTS,
+        payload: data
+       }))
+  }
+}
