691d53ab615d26a0c6a2db4bab22f12906050666
sstamenovcode <stamenov90@gmail.com>
2019-07-06 11:53:28 +0300

    made Footer component to be functional

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './Footer.scss'
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer">
-                <p>Copyright© 2019</p>
-            </footer>
-        )
-    }
+const footer = (props) => {
+    return (
+        <footer className="footer">
+            <p>Copyright© 2019</p>
+        </footer>
+    )
 }
+
+export default footer
