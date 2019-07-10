02d926ebbcd5827002da23b34eb88ae0db3d8311
sstamenovcode <stamenov90@gmail.com>
2019-07-10 10:39:17 +0300

    adjusted Toolbar component to be a functional one

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './Toolbar.scss'
 
-class Toolbar extends Component {
-    render() {
-        return (
-            <div className="header">
-                <Link to="/">
-                    <span className="logo">The Natural Dating</span>
+const toolbar = (props) => {
+    return (
+        <header className="header">
+            <Link to="/">
+                <span className="logo">The Natural Dating</span>
+            </Link>
+            <ul>
+                <Link to="/posts">
+                    <li>Blog</li>
                 </Link>
-                <ul>
-                    <Link to="/posts">
-                        <li>Blog</li>
-                    </Link>
-                    <Link to="#">
-                        <li>Your stories</li>
-                    </Link>
-                    <Link to="/about-us">
-                        <li>About us</li>
-                    </Link>
-                    <Link to="/contacts">
-                        <li>Contacts</li>
-                    </Link>
-                    <Link to="#">
-                        <li>Log in</li>
-                    </Link>
-                    <Link to="#">
-                        <li>Sign up</li>
-                    </Link>
-                </ul>
-            </div>
-        )
-    }
+                <Link to="#">
+                    <li>Your stories</li>
+                </Link>
+                <Link to="/about-us">
+                    <li>About us</li>
+                </Link>
+                <Link to="/contacts">
+                    <li>Contacts</li>
+                </Link>
+                <Link to="#">
+                    <li>Log in</li>
+                </Link>
+                <Link to="#">
+                    <li>Sign up</li>
+                </Link>
+            </ul>
+        </header>
+    )
 }
 
-export default Toolbar
+export default toolbar
