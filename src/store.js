5c15db66b9c1b6e81e5484668f98081e4e93fb6b
sstamenovcode <stamenov90@gmail.com>
2019-07-24 11:13:05 +0300

    added Redux DevTools to the project

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const initialState = {};
+
 const store = createStore(
     rootReducer, 
     initialState, 
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
