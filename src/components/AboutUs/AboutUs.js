eab9f25a2b589e1ac0634f0bc1b95ea11ec654df
sstamenovcode <stamenov90@gmail.com>
2019-07-26 12:56:38 +0300

    made improvements on AboutUs component

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -4,7 +4,13 @@ import './AboutUs.scss';
 const aboutUs = () => {
     return (
         <div className="about-us-container">
-            Lorem ipsum
+            <h1 className="heading">About us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam augue arcu, euismod eget sapien lacinia, facilisis malesuada ligula. Ut vulputate risus malesuada nibh cursus, et cursus urna sollicitudin. Fusce placerat venenatis urna at laoreet. Morbi luctus vel arcu quis venenatis. Integer a porta orci. Donec interdum dui vel ante vulputate, eu efficitur metus suscipit. Suspendisse in eros sed est vulputate dapibus at tincidunt ante. Praesent ultrices risus metus, vitae volutpat nibh convallis a. Cras sed cursus magna, ut eleifend enim. Fusce at euismod ipsum.
+            </p>
+            <p>
+                Curabitur at est libero. Praesent nec est justo. Aenean sed augue ac diam mollis egestas. Donec consequat vulputate ligula in pharetra. Ut pellentesque metus lorem, eget tempor arcu bibendum et. Vivamus eleifend placerat augue, consequat posuere mauris sagittis eget. Phasellus vulputate mauris ac nisi fringilla, eget scelerisque lacus iaculis. Aliquam nec interdum enim, a tincidunt tortor. Suspendisse enim lectus, molestie vitae vehicula vitae, porttitor at odio. Phasellus leo libero, faucibus sit amet vestibulum eu, cursus at massa. Fusce nec ante vel lorem aliquet interdum. Aliquam sit amet libero dolor. Phasellus at ultrices felis. Pellentesque ac sem mi.
+            </p>
         </div>
     )
 }
