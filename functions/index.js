7bebf26877628e42999fd7c5d052472c8f1da95d
sstamenovcode <stamenov90@gmail.com>
2019-08-21 14:56:04 +0300

    set Firestore function for adding Admin roles

--- functions/index.js
+++ functions/index.js
@@ -40,3 +40,27 @@ exports.sendMail = functions.https.onRequest((req, res) => {
         });
     });    
 });
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    // check if request is made by admin
+    if (context.auth.token.admin !== true) {
+        return { error: 'Only admins can add other admins.' };
+    }
+
+    // get user and add custom claim (admin)
+    return admin.auth()
+        .getUserByEmail(data.email)
+        .then(user => {
+            return admin.auth().setCustomUserClaims(user.uid, {
+                admin: true
+            })
+        })
+        .then(() => {
+            return {
+                message: `Success! ${data.email} has been made an admin.`
+            }
+        })
+        .catch(error => {
+            return error;
+        });
+});
