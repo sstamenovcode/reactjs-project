176cfab3808dcf0522136b4a972eeb4d7af0ffde
sstamenovcode <stamenov90@gmail.com>
2019-08-22 14:57:47 +0300

    link now redirecting to clubs pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ export class Home extends Component {
                         <h3 className="post-title">{post.title}</h3>
                         <hr />
                         <p className="post-text">{truncate(post.text)}</p>
-                        <Link to={`/posts/${post.id}`} className="read-more-container">Read more</Link>
+                        <Link to={`/clubs/${post.id}`} className="read-more-container">Read more</Link>
                     </div>
         }).slice(0, 3);
 
@@ -26,7 +26,7 @@ export class Home extends Component {
                 <div className="hero-image">
                     <h1 className="hero-text">Find information about the club you want.</h1>
                 </div>
-                <h2 className="latest-blog-posts">&#60;&#60; Latest blog posts 	&#62;&#62;</h2>
+                <h2 className="latest-blog-posts">&#60;&#60; Most recently added clubs 	&#62;&#62;</h2>
                 <div className="latest-posts-container">
                     {postItems}
                 </div>
